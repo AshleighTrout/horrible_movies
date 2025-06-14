922cb5fb574ad68a8a214240e651edf9efedc8e0
Ash <47375278+ash2983@users.noreply.github.com>
2025-06-14 14:05:35 -0500

    Adding basic html template setup

--- movie_inventory/urls.py
+++ movie_inventory/urls.py
@@ -1,6 +1,7 @@
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('inventory/', views.inventory, name='inventory'),
+    path('', views.inventory, name='inventory'),
 ]
--- movie_inventory/views.py
+++ movie_inventory/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+sample_movies = [
+    {
+        'title': 'Movie 1',
+        'year': 1999,
+        'genre': 'Action',
+        'description': 'A movie',
+        'rating': 2.0,
+        'mature': True
+    },
+{
+        'title': 'Movie 2',
+        'year': 2015,
+        'genre': 'Comedy',
+        'description': 'Another movie',
+        'rating': 2.2,
+        'mature': False
+    }
+]
+
 def inventory(request):
-    return HttpResponse("Hit inventory endpoint")
+    context = {
+        'movies': sample_movies
+    }
+    return render(request, 'home.html', context)
