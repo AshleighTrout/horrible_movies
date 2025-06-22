d85f2d08df597ad5be0eb8842219fbf8d7f9269d
Ash <ashleigh.j.trout@gmail.com>
2025-06-22 12:10:24 -0500

    Developing method to collect movies for an input provider

--- movie_inventory/inventory/inventory.py
+++ movie_inventory/inventory/inventory.py
@@ -10,7 +10,8 @@ class Inventory:
     }
 
     urls = {
-        "get_streaming_providers": "https://api.themoviedb.org/3/watch/providers/movie?language=en-US"
+        "get_streaming_providers": "https://api.themoviedb.org/3/watch/providers/movie?",
+        "get_movies_per_provider": "https://api.themoviedb.org/3/discover/movie?"
     }
 
     def __init__(self):
@@ -19,7 +20,8 @@ class Inventory:
     def get_streaming_providers(self):
         providers = {}
         url = self.urls["get_streaming_providers"]
-        response = requests.get(url, headers=self.headers)
+        params = {"language": "en-US"}
+        response = requests.get(url, headers=self.headers, params=params)
 
         provider_objects = response.json()["results"]
 
@@ -32,6 +34,27 @@ class Inventory:
 
         return providers
 
+    def get_movies_per_provider(self, provider_name):
+        collected_movies = []
+        params = {
+            "language": "en-US",
+            "with_watch_providers": provider_name,
+            "include_video": False,
+            "include_adult": False
+        }
+        url = self.urls["get_movies_per_provider"]
+        response = requests.get(url, headers=self.headers, params=params)
+        collected_movies = response.json()["results"]
+        total_pages = response.json()["total_pages"]
+        for page in range(2, total_pages + 1):
+            logging.info(f"Getting page {page} of {total_pages}")
+            params["page"] = page
+            response = requests.get(url, headers=self.headers, params=params)
+            logging.info(f"Received {len(response.json()['results'])} movies. {len(collected_movies)} collected so far.")
+            collected_movies.extend(response.json()["results"])
+
+        return collected_movies
+
     def collect_inventory(self):
         return 1
 
@@ -39,4 +62,7 @@ class Inventory:
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     inventory = Inventory()
-    inventory.get_streaming_providers()
+    movies = inventory.get_movies_per_provider("Netflix")
+
+    for movie in movies:
+        print(movie)
