49858152253c845178c99e96fa812912db4a3e71
Ash <ashleigh.j.trout@gmail.com>
2025-07-20 11:32:25 -0500

    Creating provider-specific and global bayesian constant values

--- movie_inventory/inventory/inventory.py
+++ movie_inventory/inventory/inventory.py
@@ -21,6 +21,7 @@ class Inventory:
     def __init__(self):
         self.inventory = []
         self.providers = ["Netflix"]
+        self.weighted_constants = {}
         self.average_rating = 0
         self.average_review_count = 0
         self.collected_records = 0
@@ -62,17 +63,27 @@ class Inventory:
     def compute_popularity(self, provider_name):
         print(1)
 
-    def average_primer(self, results):
+    def average_primer(self, results, provider_name):
         for result in results:
+
+            #update global constants
             self.average_rating += result['vote_average']
             self.average_review_count += result['vote_count']
             self.collected_records += 1
 
-    def finalize_bayesian_constants(self):
+            #update provider constants
+            self.weighted_constants[provider_name]["average_rating"] += result['vote_average']
+            self.weighted_constants[provider_name]["average_review_count"] += result['vote_count']
+            self.weighted_constants[provider_name]["collected_records"] += 1
+
+
+    def finalize_provider_bayesian_constants(self, provider_name):
+        self.weighted_constants[provider_name]["average_rating"] = self.weighted_constants[provider_name]["average_rating"] / self.weighted_constants[provider_name]["collected_records"]
+        self.weighted_constants[provider_name]["average_review_count"] =self.weighted_constants[provider_name]["average_review_count"] / self.weighted_constants[provider_name]["collected_records"]
+
+    def finalize_global_bayesian_constants(self):
         self.average_rating = self.average_rating / self.collected_records
         self.average_review_count = self.average_review_count / self.collected_records
-
-
 
     def fragmented_inventory(self, provider_name):
         collected_movies = []
@@ -93,7 +104,7 @@ class Inventory:
                     output = response.json()["results"]
                     collected_movies.append(output)
                     logging.info(f"Collected {len(output)} movies for Provider: {provider_name} across {year}-{month} to {year}-{month+1}")
-                    self.average_primer(output)
+                    self.average_primer(output, provider_name)
                 except Exception as e:
                     logging.error(f"Failed to collect {provider_name} for {year}-{month}-01 to {year}-{month + 1}-01 :: {e}")
 
@@ -102,17 +113,22 @@ class Inventory:
     def generate_inventory(self):
         result = {}
         for provider in self.providers:
+            self.weighted_constants[provider] = {
+                "average_rating": 0,
+                "average_review_count": 0,
+                "collected_records": 0,
+            }
             fragmentation_flag = self.initial_request(provider)
             if fragmentation_flag:
                 result[provider] = self.fragmented_inventory(provider)
             else:
                 result[provider] = self.get_movies_per_provider(provider)
-            self.finalize_bayesian_constants()
-            logging.info(f"Average Rating = {self.average_rating}\nAverage Review Count = {self.average_review_count}\nCollected Records = {self.collected_records}")
+            self.finalize_provider_bayesian_constants(provider)
+        self.finalize_global_bayesian_constants()
+        logging.info(f"Average Rating = {self.average_rating}\nAverage Review Count = {self.average_review_count}\nCollected Records = {self.collected_records}")
         return result
 
     def get_movies_per_provider(self, provider_name):
-        collected_movies = []
         params = {
             "language": "en-US",
             "with_watch_providers": provider_name,
@@ -129,11 +145,8 @@ class Inventory:
             response = requests.get(url, headers=self.headers, params=params)
             logging.info(f"Received {len(response.json()['results'])} movies. {len(collected_movies)} collected so far.")
             collected_movies.extend(response.json()["results"])
-            self.average_primer(response.json()["results"])
+            self.average_primer(response.json()["results"], provider_name)
         return collected_movies
-
-    def collect_inventory(self):
-        return 1
 
 
 if __name__ == "__main__":
